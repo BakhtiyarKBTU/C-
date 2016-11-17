0ba197c32ab675c20166d9d5f0da7ff7c90225f2
Sabo <bakhtiyarkbtu@gmail.com>
2016-11-17 13:01:01 +0600

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+const int N = 1001;
+
+int a[N], n;
+
+int main(){
+	cin >> n;
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}	
+//	sort(a, a + n);
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			if(a[i] > a[j]) swap(a[i], a[j]);
+		}	
+	}  
+	for(int i = 0; i < n; i++) cout << a[i] << ' ';
+	return 0;
+}
